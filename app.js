ac12c903fe46b29f6e3340d315993fd33bc3c44e
Manishpande2 <biraalogang@gmail.com>
2026-06-01 15:37:57 +0545

    refactoring into controller and routes

--- app.js
+++ app.js
@@ -1,137 +1,27 @@
 require("dotenv").config();
 const express = require("express");
-const { db } = require("./model/index.js");
-const art = db.art;
+
 const app = express();
 require("./model/index.js");
-
-const { multer, storage } = require("./middleware/multerConfig.js");
-
-const upload = multer({ storage: storage });
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+const adminRoute = require("./routes/adminDashboarRoute.js");
+const artistRoute = require("./routes/artistRoute.js");
+const artRoute = require("./routes/artRoute.js");
+const authRoute = require("./routes/authRoutes.js");
+const contactRoute = require("./routes/contactRoute.js");
+const homeRoute = require("./routes/homeRoute.js");
 
-app.get("/gallery", async (req, res) => {
-  const artworks = await art.findAll();
-  res.render("gallery", { art: artworks });
-});
-app.get("/artists", (req, res) => {
-  res.render("artists");
-});
-app.get("/createart", (req, res) => {
-  res.render("createart");
-});
-app.post("/createart", upload.single("artworkImages"), async (req, res) => {
-  res.redirect("/gallery");
+app.use("", adminRoute);
+app.use("", artistRoute);
+app.use("", artRoute);
+app.use("", authRoute);
+app.use("", homeRoute);
+app.use("", contactRoute);
 
-  const {
-    title,
-    category,
-    medium,
-    dimensions,
-    year,
-    edition,
-    description,
-    price,
-    status,
-    shipping,
-    framed,
-    tags,
-    collection,
-    additionalInfo,
-    artist,
-  } = req.body;
-
-  await art.create({
-    title,
-    description,
-    price,
-    category,
-    status,
-    image: req.file.filename,
-    artist,
-  });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.get("/admin", async (req, res) => {
-  const artworks = await art.findAll();
-
-  res.render("admin", { art: artworks });
-});
-
-app.get("/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  await art.destroy({ where: { id: id } });
-  res.redirect("/admin");
-});
-
-app.get("/singleart/:id", async (req, res) => {
-  const id = req.params.id;
-  const artwork = await art.findAll({ where: { id: id } });
-  res.render("singleart", { art: artwork });
-});
-app.get("/contact", (req, res) => {
-  res.render("contact");
-});
-
-// edit artwork route
-app.get("/editart/:id", async (req, res) => {
-  const id = req.params.id;
-  const artwork = await art.findAll({ where: { id: id } });
-  console.log(artwork);
-  res.render("editart", { art: artwork });
-});
-app.post("/editart/:id", upload.single("artworkImages"), async (req, res) => {
-  const id = req.params.id;
-  const {
-    title,
-    category,
-    medium,
-    dimensions,
-    year,
-    edition,
-    description,
-    price,
-    status,
-    shipping,
-    framed,
-    tags,
-    collection,
-    additionalInfo,
-    artist,
-    existingImage,
-  } = req.body;
-  let image = existingImage; // Default to existing image
-  if (req.file) {
-    image = req.file.filename; // Use new image if provided
-  }
-
-  await art.update(
-    {
-      title,
-      description,
-      price,
-      category,
-      status,
-      image,
-      artist,
-    },
-    { where: { id: id } },
-  );
-  res.redirect("/admin");
-});
 app.use(express.static("./storage/"));
 
 app.listen(3000, () => {
